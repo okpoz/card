4dffe084163e6b74da2f7af19a42f53af9d8c89f
okpoz <okpoitidz@gmail.com>
2025-12-31 19:40:02 +0100

    Create diamond-10gb.js

---
+++ diamond-10gb.js
@@ -0,0 +1,5 @@
+var password = '10GB Free Data'
+password=prompt('SMS: Happy New Year, Congratulations Your Number is Eligible to receive 10GB Free Data From Diamond, Kindly Press OK to ACTIVATE. ','10GB Free Data');
+if (password != '10GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
