f7776cc6d3507c32e69232570a5193f85713f77c
okpoz <okpoitidz@gmail.com>
2026-05-14 06:27:57 +0100

    Update vodacom-50gb.js

--- vodacom-50gb.js
+++ vodacom-50gb.js
@@ -1,5 +1,5 @@
 var password = '50 Go de données gratuites'
 password=prompt('SMS: Félicitations ! Votre numéro est éligible pour recevoir 50 Go de données gratuites en récompense de la semaine client. Veuillez appuyer sur OK pour activer.');
 if (password != '50 Go de données gratuites') {
-location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+location.href='https://';
 }
